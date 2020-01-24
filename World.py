216063846d8c5105f026816281d35d99ed6a5291
Arun Kumar <arun89@terpmail.umd.edu>
2020-01-23 23:28:12 -0500

    Added crop planting feature to json file as well as rover location fetching functionality based on current location

--- World.py
+++ World.py
@@ -4,6 +4,7 @@ import math
 import re
 import operator
 import time
+import pprint
 
 from functools import reduce
 
@@ -14,6 +15,14 @@ def get_location(nested_dict=None, keys_list=None):
     if nested_dict is None:
         nested_dict = {}
     return reduce(operator.getitem, keys_list, nested_dict)
+
+
+def update_crops(nested_dict=None, keys_list=None, crop="CARROTS"):
+    if keys_list is None:
+        keys_list = []
+    if nested_dict is None:
+        nested_dict = {}
+    get_location(nested_dict, keys_list[:-1])[keys_list[-1]] = crop
 
 
 # TODO Create docstrings
@@ -31,6 +40,7 @@ class World:
             try:
                 with open(self.json_path) as json_file:
                     self.data = json.load(json_file)
+                    pprint.pprint(self.data)
             except FileNotFoundError:
                 print("The file  ./maps/coding_challenge.json does not exist\n"
                       "Retrying to load the same file again")
@@ -53,18 +63,37 @@ class World:
         for item in self.path_lengths:
             print(item)
 
-    def decrypt_location(self, location="FBR8"):
-        print(location)
+    def decrypt_location(self, location="Charger"):
         if re.match(r'(\s)*charger(\s)*', location, re.IGNORECASE):
-            return self.data['world']['charger']['location']
-        elif re.match(r'(\s)*FBR(\s)*\d+', location, re.IGNORECASE):
-            output = "{0:0=2d}".format(int((re.findall(r'\d+', location))[0]))
-            key = ["world", "fields", "field-b", "rows", "row-" + str(output), "location"]
-            return get_location(self.data, key)
+            return "[charger]", self.data['world']['charger']['location']
+        elif re.match(r'(\s)*PTH(\s)*\d+', location, re.IGNORECASE):
+            output = (re.findall(r'\d+', location))
+            path_number = int(output[0])
+            waypoint_number = int(output[1])
+            location_key = ["world", "paths", "path-" + str(path_number), "waypoint-" + str(waypoint_number)]
+            return "[path-" + str(path_number) + "]", get_location(self.data, location_key)
         else:
             output = "{0:0=2d}".format(int((re.findall(r'\d+', location))[0]))
-            key = ["world", "fields", "field-a", "rows", "row-" + str(output), "location"]
-            return get_location(self.data, key)
+            location_key = ["world", "fields",
+                            "field-b" if re.match(r'(\s)*FBR(\s)*\d+', location, re.IGNORECASE) else "field-a", "rows",
+                            "row-" + str(output), "location"]
+            return "[" + str(location_key[2]) + "]" + "[" + str(location_key[4]) + "]", get_location(self.data,
+                                                                                                     location_key)
+
+    def update_world(self, location="FBR8", crop="CARROTS"):
+        if re.match(r'(\s)*FBR(\s)*\d+', location, re.IGNORECASE):
+            output = "{0:0=2d}".format(int((re.findall(r'\d+', location))[0]))
+            crop_key = ["world", "fields", "field-b", "rows", "row-" + str(output), "crop"]
+            update_crops(self.data, crop_key, crop)
+        else:
+            output = "{0:0=2d}".format(int((re.findall(r'\d+', location))[0]))
+            crop_key = ["world", "fields", "field-a", "rows", "row-" + str(output), "crop"]
+            update_crops(self.data, crop_key, crop)
+
+    def write_to_world_file(self):
+        world_file = open(self.json_path, "w+")
+        world_file.write(json.dumps(self.data))
+        world_file.close()
 
 
 if __name__ == '__main__':
