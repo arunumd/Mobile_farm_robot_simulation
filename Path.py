fe00fb123ccbde7949a400877bb80d76a90e05df
Arun Kumar <arun89@terpmail.umd.edu>
2020-01-23 23:26:58 -0500

    Removed redundant waypoint from current location

--- Path.py
+++ Path.py
@@ -6,7 +6,7 @@ from World import World
 
 
 def set_locations(current_location="FBR01",
-                  next_location="FAR15"):
+                  next_location="Charger"):
     def get_tuple(location="FBR01"):
         # Code for current location matches
         if re.match(r'FBR\d+', location):
@@ -31,15 +31,22 @@ class Path:
     def find_path(self, locations=()):
         present_location = list(locations[0:2])
         goal_location = list(locations[2:4])  # If start and goals are in the same fields
+        print(present_location, goal_location)
         if goal_location[0] == present_location[0]:
+            """
+            Description:
+            -----------
+            Condition to find path when the goal node and current node are in the same field
+            """
             if goal_location[0] == 'FBR' or goal_location[0] == 'FAR':
                 prefix = goal_location[0]
+                # Goal row is higher than present row
                 if int(goal_location[1]) - int(present_location[1]) >= 0:
                     return deque([prefix + " " + str(i)
-                                  for i in (range(present_location[1], goal_location[1] + 1))])
+                                  for i in (range(present_location[1] + 1, goal_location[1] + 1))])
                 else:
                     return deque([prefix + " " + str(i)
-                                  for i in (range(present_location[1], goal_location[1] + 1, -1))])
+                                  for i in (range(present_location[1] - 1, goal_location[1] - 1, -1))])
 
         # If start and goals are in different fields
         elif (goal_location[0] == 'FAR' or 'FBR') and (present_location[0] == 'FAR' or 'FBR') and (
@@ -61,15 +68,18 @@ class Path:
 
             if goal_location[0] == 'FAR':
                 print(present_location, goal_location)
-                return path_b_to_a(current_coordinate=present_location[1],
-                                   future_coordinate=goal_location[1], prepend=prefix)
-            # Going from field A to field B
+                final_path = path_b_to_a(current_coordinate=present_location[1],
+                                         future_coordinate=goal_location[1], prepend=prefix)
+                final_path.popleft()
+                return final_path
+                # Going from field A to field B
             else:
                 print(present_location, goal_location)
-                path_ab = path_b_to_a(current_coordinate=goal_location[1],
-                                      future_coordinate=present_location[1])
-                path_ab.reverse()
-                return path_ab
+                final_path = path_b_to_a(current_coordinate=goal_location[1],
+                                         future_coordinate=present_location[1])
+                final_path.reverse()
+                final_path.popleft()
+                return final_path
 
         # If robot has to travel from the fields to the charger
         else:
@@ -79,12 +89,12 @@ class Path:
                     path_to_charger = deque([origin + " " + str(i)
                                              for i in (range(current_coordinate, 19))])
                     path_to_charger += deque(
-                        [("PTH05" if origin == 'FAR' else "PTH04") + " " + str(i) for i in (range(10, 0, -1))])
+                        [("PTH05" if origin == 'FAR' else "PTH04") + " " + str(i) for i in (range(10, -1, -1))])
                 else:
                     path_to_charger = deque([origin + " " + str(i)
                                              for i in (range(current_coordinate, 0, -1))])
                     path_to_charger += deque(
-                        [("PTH03" if origin == 'FAR' else "PTH01") + " " + str(i) for i in (range(10, 0, -1))])
+                        [("PTH03" if origin == 'FAR' else "PTH01") + " " + str(i) for i in (range(10, -1, -1))])
 
                 if origin == 'FAR':
                     return path_to_charger + deque(["PTH00" + " " + str(i) for i in (range(10, -1, -1))])
@@ -97,17 +107,25 @@ class Path:
                     # Go by path 5 if going by path 3 is longer
                     if self.world.path_lengths[3] + present_location[1] - 1 > \
                             self.world.path_lengths[5] + 19 - present_location[1]:
-                        return path_field_to_charger(present_location[1])
+                        final_path = path_field_to_charger(present_location[1])
+                        final_path.popleft()
+                        return final_path
                     # Otherwise go by path 3
                     else:
-                        return path_field_to_charger(present_location[1], option=1)
+                        final_path = path_field_to_charger(present_location[1], option=1)
+                        final_path.popleft()
+                        return final_path
                 else:
                     if self.world.path_lengths[1] + present_location[1] - 1 > \
                             self.world.path_lengths[4] + 19 - present_location[1]:
-                        return path_field_to_charger(present_location[1], origin='FBR')
+                        final_path = path_field_to_charger(present_location[1], origin='FBR')
+                        final_path.popleft()
+                        return final_path
                     # Otherwise go by path 3
                     else:
-                        return path_field_to_charger(present_location[1], origin='FBR', option=1)
+                        final_path = path_field_to_charger(present_location[1], origin='FBR', option=1)
+                        final_path.popleft()
+                        return final_path
 
             else:
                 print("start is charger ", present_location, goal_location)
@@ -116,20 +134,24 @@ class Path:
                             self.world.path_lengths[5] + 19 - goal_location[1]:
                         mirror_path = path_field_to_charger(goal_location[1])
                         mirror_path.reverse()
+                        mirror_path.popleft()
                         return mirror_path
                     else:
                         mirror_path = path_field_to_charger(goal_location[1], option=1)
                         mirror_path.reverse()
+                        mirror_path.popleft()
                         return mirror_path
                 else:
                     if self.world.path_lengths[1] + goal_location[1] - 1 > \
                             self.world.path_lengths[4] + 19 - goal_location[1]:
                         mirror_path = path_field_to_charger(goal_location[1], origin='FBR')
                         mirror_path.reverse()
+                        mirror_path.popleft()
                         return mirror_path
                     else:
                         mirror_path = path_field_to_charger(goal_location[1], origin='FBR', option=1)
                         mirror_path.reverse()
+                        mirror_path.popleft()
                         return mirror_path
 
 
