9ed323006bb66464fc72672f5b31ac096510707a
arunumd <arun89@terpmail.umd.edu>
2020-01-28 16:22:44 -0500

    Modified launcher script and also created an empty readme file for project description

--- Launcher.py
+++ Launcher.py
@@ -3,16 +3,10 @@ from Controller import Controller
 
 
 def launch_environment():
-    while True:
-        user_command = ""
-        try:
-            user_command = input("Please enter your command: ")
-        except ValueError:
-            print("That is not a valid command")
-            continue
-        finally:
-            if user_command.lower().strip() == "exit":
-                break
+    print("At any time if you would like to exit the algorithm, then please type 'Ctrl' + 'C'"
+          "in the console")
+    controller = Controller()
+    controller.trigger_nodes()
 
 
 if __name__ == "__main__":
